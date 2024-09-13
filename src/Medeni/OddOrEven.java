8012ad1d2a0150c01e89df1281e1043d1867f289
Medeni Rostem <mrostem04@gmail.com>
2024-09-12 19:21:42 -0500

    code updated

--- src/Medeni/OddOrEven.java
+++ src/Medeni/OddOrEven.java
@@ -3,7 +3,7 @@ package Medeni;
 public class OddOrEven {
     public static void main(String[] args) {
 
-            int n=124;
+            int n=5;
         //for (int n = 1; n <= 1; n++) {
             if (n % 2 == 0) {
                 System.out.println("Even number");
