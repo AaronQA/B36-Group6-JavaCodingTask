96be231337a1fd48189f799e08d02fb29d2b5da2
kekomartin5 <kapg25@gmail.com>
2024-09-25 00:00:17 -0400

    OddOrEven is solved

--- src/Keyko/OddOrEven.java
+++ src/Keyko/OddOrEven.java
@@ -2,7 +2,24 @@ package Keyko;
 
 public class OddOrEven {
 
+    public static void main(String[] args) {
 
+        System.out.println(identify(5));
+        System.out.println(identify(6));
+        System.out.println(identify(0));
+        System.out.println(identify(-3));
+        System.out.println(identify(-10));
+
+    }
+
+    public static String identify(int num) {
+        if (num % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
+
+    }
 }
 /*Write  a method which can identify given int number is even or odd
     Output ex:
