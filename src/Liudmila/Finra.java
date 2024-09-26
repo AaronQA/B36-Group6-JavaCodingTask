772a7f0e65761d9896cf270ed03cb191dc8c203f
LiudmilaDZ <demidovalel@gmail.com>
2024-09-25 21:45:51 -0400

    added FINRA method2

--- src/Liudmila/Finra.java
+++ src/Liudmila/Finra.java
@@ -2,22 +2,42 @@ package Liudmila;
 
 public class Finra {
     public static void main(String[] args) {
-        printFinra();
+        try{
+            printFinra(0, -8);
+        }catch(RuntimeException e){
+            e.printStackTrace();
+        }
+        System.out.println("============================");
+        printFinra(0, 12);
+        System.out.println("============================");
+        printFinra(-20, 1);
+        System.out.println("============================");
+        try{
+            printFinra(3, 1);
+        }catch(RuntimeException e){
+            e.printStackTrace();
+        }
     }
 
 
-    public static void printFinra() {
-        for (int i = 1; i <= 30; i++) {
+    public static void printFinra(int start, int end) {
+
+        if (end <= start) {
+            throw new IllegalArgumentException("Start must be less than end");
+
+        }
+        for (int i = start; i <= end; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FIN");
+                System.out.print("FINRA");
             } else if (i % 3 == 0) {
-                System.out.println("RA");
-            } else if (i % 5 == 0 ) {
-                System.out.println("FINRA");
+                System.out.print("FIN" + " ");
+            } else if (i % 5 == 0) {
+                System.out.print("RA" + " ");
             } else {
-                System.out.println(i);
+                System.out.print(i + " ");
             }
         }
+        System.out.println();
     }
 }
 //Write a method which prints out the numbers from 1 to 30 but for numbers which are a multiple of 3,
