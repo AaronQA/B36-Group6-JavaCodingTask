d08fe7835f911685849ff88b311824cc0dd111da
HadiHarmandar <hadiharmandar1@gmail.com>
2024-09-28 09:17:44 -0500

    Swap two number question solved. Added method

--- src/hadi/SwapTwoNumbers.java
+++ src/hadi/SwapTwoNumbers.java
@@ -14,5 +14,14 @@ public class SwapTwoNumbers {
         x = x - y;
 
         System.out.println("After swap; x: " + x + " y: " + y);
+
+        swapNumbers(10, 15);
+    }
+
+    static void swapNumbers(int num1, int num2) {
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("num1: " + num1 + " num2: " + num2);
     }
 }
