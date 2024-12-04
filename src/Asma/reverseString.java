c3f8826d6c4b3259f868577403ca20333493df5b
asma <asmabaker94@gmail.com>
2024-12-04 15:28:08 -0500

    second solution

--- src/Asma/reverseString.java
+++ src/Asma/reverseString.java
@@ -5,22 +5,36 @@ public class reverseString {
     public static void main(String[] args) {
 
 
-        System.out.println(reverse("Testing is fun"));
+        System.out.println(reverse1("Testing is fun"));
+        System.out.println(reverse2("Testing is fun"));
 
     }
 
-    public static String reverse(String word){
+    public static String reverse1(String word) {
 
-        String result ="";
-        if(word.isEmpty()||word.isBlank()){
+        String result = "";
+        if (word.isEmpty() || word.isBlank()) {
             throw new IllegalArgumentException("Input cannot be empty or blank");
-
-
-        }for(int i =word.length()-1; i>=0;i--){
-            result+=word.charAt(i);
+        }
+        for (int i = word.length() - 1; i >= 0; i--) { // gets the index of the last character
+            result += word.charAt(i); // retrieves the character at index i and added to result to build the result
         }
         return result;
     }
 
 
+    //Solution with StringBuilder for efficient string manipulation
+    //In solution 1 concatenating strings in a loop using += is inefficient cuz creates a new string object each time
+    public static String reverse2(String word) {
+        if (word == null || word.isBlank()) {
+            throw new IllegalArgumentException("Input cannot be null, empty, or blank");
+        }
+        // Use StringBuilder to reverse the word using method
+        StringBuilder result = new StringBuilder(word);
+        return result.reverse().toString(); //to explicitly convert the StringBuilder to a String.
+    }
 }
+
+
+
+
